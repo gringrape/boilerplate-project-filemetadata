5b73af61c6d0eef7a027d30654a1a6b0671bc09e
gringrape <gringrape200@gmail.com>
2020-06-15 16:54:14 +0900

    finish

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var fileUpload = require('express-fileupload');
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(fileUpload());
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
@@ -16,6 +16,16 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', (req, res) => {
+  const file = req.files['upfile']; 
+  res.json(
+    {
+      name: file.name,
+      size: file.size
+    }
+  )
 });
 
 app.listen(process.env.PORT || 3000, function () {
